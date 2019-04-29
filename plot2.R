1aea53962b025f8b3c8294de39aa8410c42f2bbe
YOB <yigitozanberk@gmail.com>
2019-04-29 16:47:49 +0300

    cp rest

--- plot2.R
+++ plot2.R
@@ -6,7 +6,8 @@ data <- data[which(data$Date == as.Date("2007-02-01") | data$Date == as.Date("20
 data[,1] <- paste(data[,1], data[,2])
 data[,1] <- as.POSIXct(data[,1])
 
-plot(data$Date, data$Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
+plot(data$Date, data$Global_active_power, type = "l", 
+     ylab = "Global Active Power (kilowatts)", xlab = "")
 
 dev.copy(png, file= "plot2.png", width = 480, height = 480)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -5,6 +5,22 @@ data <- data[which(data$Date == as.Date("2007-02-01") | data$Date == as.Date("20
 
 data[,1] <- paste(data[,1], data[,2])
 data[,1] <- as.POSIXct(data[,1])
+#plots
+
+plot.new()
+#setting mfrow par
+par(mfrow = c(2,2), mar = c(4,4,2,1))
+
+#first plot
+plot(data$Date, data$Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
+
+
+#second plot
+plot(data$Date, data$Voltage, 
+     ylab = "Voltage", xlab = "datetime",
+     type = "l")
+
+#third plot
 
 plot(data$Date, data$Sub_metering_1, type = "n", ylab = "Energy Sub Metering", xlab = "")
 
@@ -20,9 +36,12 @@ points(data$Date, data$Sub_metering_2, col = "red", type = "l")
 
 points(data$Date, data$Sub_metering_3, col = "blue", type = "l")
 
+#fourth plot
 
-dev.copy(png, file= "plot3.png", width = 480, height = 480)
+plot(data$Date, data$Global_reactive_power, 
+     ylab = "Global_reactive_power",
+     xlab = "datetime",
+     type = "l")
+
+dev.copy(png, file= "plot4.png", width = 480, height = 480)
 dev.off()
-
-
-
